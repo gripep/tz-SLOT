66369a4f318e7324f003fbb6782ec583e46b0ab6
gripepi <gian.ripepi96@gmail.com>
2020-06-10 22:20:31 +0100

    React setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./assets/css/react.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TZ Stats, baby!</h1>
+    </div>
+  );
+}
+
+export default App;
