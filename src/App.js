489157d3a1e432dc5032265137ea6643a5973c69
gripepi <gian.ripepi96@gmail.com>
2020-06-22 21:47:35 +0100

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Stats from "./components/Stats";
 
 import "./assets/css/chart-react.css";
@@ -8,6 +9,7 @@ import "./assets/css/chart-react.css";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Stats />
       <Footer />
     </div>
