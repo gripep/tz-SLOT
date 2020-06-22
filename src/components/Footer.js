cc58aef5d8f093a618fe00bd3e5118daaaf48175
gripepi <gian.ripepi96@gmail.com>
2020-06-22 19:11:47 +0100

    Added link to Tezos API

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,14 +23,14 @@ export class Footer extends Component {
             <Row className="row-grid align-items-center mb-1">
               <Col lg="6">
                 <h4 className="mb-0 font-weight-light">
-                  Find out more about this project on Github!
+                  Find out more on Github!
                 </h4>
               </Col>
               <Col className="text-lg-center btn-wrapper" lg="6">
                 <Button
                   className="btn-icon-only rounded-circle ml-1"
                   color="github"
-                  href="https://github.com/gripep/tzstats"
+                  href="https://github.com/gripep"
                   id="tooltip495507257"
                   target="_blank"
                 >
@@ -52,6 +52,14 @@ export class Footer extends Component {
               </Col>
               <Col md="6">
                 <Nav className=" nav-footer justify-content-end">
+                  <NavItem>
+                    <NavLink
+                      href="https://tzstats.com/docs/api/index.html"
+                      target="_blank"
+                    >
+                      Tezos API
+                    </NavLink>
+                  </NavItem>
                   <NavItem>
                     <NavLink
                       href="https://github.com/gripep/tzstats/blob/master/LICENSE.md"
