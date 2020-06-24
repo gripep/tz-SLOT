6333db03c48804d6611b28f9678a97616d67250d
gripepi <gian.ripepi96@gmail.com>
2020-06-24 13:38:49 +0200

    Added CORS policy to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
+
 app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -12,5 +13,14 @@ app.get("/ping", function (req, res) {
 });
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+// overcome CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 app.listen(port);
