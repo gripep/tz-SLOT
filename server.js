dc168b591b2b87d071aedc10007630ffbd7f1c64
gripepi <gian.ripepi96@gmail.com>
2020-06-24 13:50:45 +0200

    Added CORS calls

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
+var cors = require("cors");
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -8,19 +10,24 @@ app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
+// CORS
+app.use(cors());
+var corsOptions = {
+  origin: function (origin, callback) {
+    db.loadOrigins(function (error, origins) {
+      callback(error, origins);
+    });
+  },
+};
+// app.get("/ping", function (req, res) {
+//   return res.send("pong");
+// });
+app.get("api.tzstats.com", cors(corsOptions), function (req, res) {
+  return res;
 });
-app.get("/*", function (req, res) {
+
+https: app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-// overcome CORS
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.listen(port);
