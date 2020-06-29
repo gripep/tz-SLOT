e4779962c8fae8d5ba5a0724d87624436285e575
gripepi <gian.ripepi96@gmail.com>
2020-06-30 00:11:18 +0200

    Added Landing component && temp CSS

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 import Footer from "./components/Footer";
 // import Header from "./components/Header";
+import Landing from "./components/Landing";
 import Stats from "./components/Stats";
 
-import "./assets/css/chart-react.css";
+// import "./assets/css/chart-react.css";
+import "./assets/css/argon-dashboard-react.css";
 
 function App() {
   return (
@@ -15,8 +17,8 @@ function App() {
         {/* <Header /> */}
         <div className="App">
           <Switch>
-            {/* <Route exact path="/" component={Landing} /> */}
-            <Route exact path="/" component={Stats} />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/single" component={Stats} />
             <Redirect to="/" />
           </Switch>
         </div>
