8a6c76a72b34b1c00182ba68322dc6e53d993409
Gianfranco Ripepi <gianfranco.ripepi@plentific.com>
2020-07-26 18:20:59 +0200

    Created bakers api lambda fn for prod

---
+++ functions/baker.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+
+exports.handler = (event, context, callback) => {
+  // send response
+  const send = (body) => {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+      },
+      body: JSON.stringify(body),
+    });
+  };
+
+  // perform API call
+  const getBakers = () => {
+    axios
+      .get("https://api.tzkt.io/v1/suggest/accounts/")
+      .then((res) => {
+        send(res.data);
+      })
+      .catch((err) => {
+        send(err.data);
+      });
+  };
+
+  // make sure method is POST
+  if (event.httpMethod == "GET") {
+    getBakers();
+  }
+};
